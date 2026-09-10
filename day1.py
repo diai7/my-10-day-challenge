eb0d37f54a6d6fc119e7c0e50a07fe37ef2a2086
diai7 <didaraiybekova6@gmail.com>
2026-09-10 20:18:39 +0300

    Day 1: add test calls

--- day1.py
+++ day1.py
@@ -15,3 +15,6 @@ def calculator(a, b, operator):
         if b == 0:
             return "Error: division by zero"
         return a / b
+
+print(calculator(5, 3, "+"))
+print(calculator(10, 0, "/"))
