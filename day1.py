e04d3693021da1003e01cb78c694de3dda87717f
diai7 <didaraiybekova6@gmail.com>
2026-09-10 20:30:32 +0300

    Fix calculator code

--- day1.py
+++ day1.py
@@ -1,7 +1,4 @@
 def calculator(a, b, operator):
-    pass
-
-def calculator(a, b, operator):
     if operator == "+":
         return a + b
     elif operator == "-":
@@ -9,12 +6,12 @@ def calculator(a, b, operator):
     elif operator == "*":
         return a * b
     elif operator == "/":
-        return a / b
-
-    elif operator == "/":
         if b == 0:
             return "Error: division by zero"
         return a / b
+    else:
+        return "Error: unknown operator"
+
 
 print(calculator(5, 3, "+"))
 print(calculator(10, 0, "/"))
