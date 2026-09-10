495638ecf92f50604ba31f20d79fa4c1bd2565de
diai7 <didaraiybekova6@gmail.com>
2026-09-10 20:17:25 +0300

    Day 1: handle division by zero

--- day1.py
+++ day1.py
@@ -10,3 +10,8 @@ def calculator(a, b, operator):
         return a * b
     elif operator == "/":
         return a / b
+
+    elif operator == "/":
+        if b == 0:
+            return "Error: division by zero"
+        return a / b
